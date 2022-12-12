2d4e2add7778990871530b457283a3c0815a4d04
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-12-12 21:06:32 +0000

    That worked

--- terraform/data.log_analytics._workspace.tf
+++
@@ -1,6 +0,0 @@
-#data "azurerm_log_analytics_workspace" "platform" {
-#  provider            = azurerm.log_analytics
-#
-#  name                = var.log_analytics_workspace_name
-#  resource_group_name = var.log_analytics_resource_group_name
-#}
