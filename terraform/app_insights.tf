061219c00fd92fd3d9f76fc763abd3f2bd37fb86
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-12-12 20:50:20 +0000

    Build ID rather than reference

--- terraform/app_insights.tf
+++ terraform/app_insights.tf
@@ -2,7 +2,8 @@ resource "azurerm_application_insights" "ai" {
   name                = local.app_insights_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  workspace_id        = data.azurerm_log_analytics_workspace.platform.id
+  //workspace_id        = data.azurerm_log_analytics_workspace.platform.id
+  workspace_id = "/${var.log_analytics_subscription_id}/resourceGroups/${var.log_analytics_resource_group_name}/providers/Microsoft.OperationalInsights/workspaces/${var.log_analytics_workspace_name}"
 
   application_type    = "web"
 }
