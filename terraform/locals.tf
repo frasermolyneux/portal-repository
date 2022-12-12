0fc367583755b519c19e443ea6897ad807eb5899
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-12-12 20:17:16 +0000

    Fix KV name

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,4 +1,4 @@
 locals {
   resourceGroupName = "rg-portal-repository-${var.environment}-${var.location}"
-  keyVaultName = "kv-${var.environment}-${var.location}"
+  keyVaultName = "kv-${var.environment}-${random_id.environment_id.hex}"
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -16,3 +16,7 @@ provider "azurerm" {
 }
 
 data "azurerm_client_config" "current" {}
+
+resource "random_id" "environment_id" {
+    byte_length = 6
+}
