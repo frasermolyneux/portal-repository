6be8b8efc842d89bf0b7be4f5b59336afef18bc1
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-12-13 19:17:53 +0000

    Add storage

--- terraform/app_insights_key_vault_secrets.tf
+++ terraform/app_insights_key_vault_secrets.tf
@@ -1,11 +1,11 @@
 resource "azurerm_key_vault_secret" "app_insights_connection_string_secret" {
-  name         = "${azurerm_application_insights.ai.name}-connectionstring"
+  name         = format("%s-connectionstring", azurerm_application_insights.name)
   value        = azurerm_application_insights.ai.connection_string
   key_vault_id = azurerm_key_vault.kv.id
 }
 
 resource "azurerm_key_vault_secret" "app_insights_instrumentation_key_secret" {
-  name         = "${azurerm_application_insights.ai.name}-instrumentationkey"
+  name         = format("%s-instrumentationkey", azurerm_application_insights.ai.name)
   value        = azurerm_application_insights.ai.instrumentation_key
   key_vault_id = azurerm_key_vault.kv.id
 }
--- terraform/locals.tf
+++ terraform/locals.tf
@@ -4,4 +4,5 @@ locals {
   app_insights_name = "ai-ptl-repo-${random_id.environment_id.hex}-${var.environment}-${var.location}"
   workload_name = "ptl-repo-${random_id.environment_id.hex}-${var.environment}"
   web_app_name = "webapi-ptl-repo-${random_id.environment_id.hex}-${var.environment}-${var.location}"
+  app_data_storage_name = "saptlrepoad${random_id.environment_id.hex}${var.location}"
 }
