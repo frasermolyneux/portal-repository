60ea560c346da8b8ff822b1415f8a267c7ab5596
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-12-18 13:44:43 +0000

    Fix resource reference

--- terraform/key_vault.tf
+++ terraform/key_vault.tf
@@ -33,5 +33,5 @@ resource "azurerm_role_assignment" "deploy_principal_kv_role_assignment" {
 resource "azurerm_role_assignment" "web_app_kv_role_assignment" {
   scope                = azurerm_key_vault.kv.id
   role_definition_name = "Key Vault Secrets User"
-  principal_id         = data.azurerm_linux_web_app.app.identity.0.principal_id
+  principal_id         = azurerm_linux_web_app.app.identity.0.principal_id
 }
