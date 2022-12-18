981756084952969f03ac67add708f94918eb38ce
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-12-18 14:50:56 +0000

    Fix provider

--- terraform/data.sql_server.tf
+++ terraform/data.sql_server.tf
@@ -1,5 +1,5 @@
 data "azurerm_mssql_server" "platform" {
-  provider            = "sql"
+  provider            = azurerm.sql
   name                = var.sql_server_name
   resource_group_name = var.sql_resource_group_name
 }
