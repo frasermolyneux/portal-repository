1414af67b00c9d747aaaaefb5a58fd8860396ed1
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-12-19 20:58:15 +0000

    Clean-up

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,7 @@
 output "workload_public_url" {
-  value = azurerm_cdn_frontdoor_custom_domain.app.host_name
+  value = format("https://%s", azurerm_cdn_frontdoor_custom_domain.app.host_name)
 }
 
 output "web_app_name" {
   value = azurerm_linux_web_app.app.name
-}
-
-output "web_app_resource_group" {
-  value = var.web_apps_resource_group_name
-}
-
-output "web_app_subscription_id" {
-  value = var.web_apps_subscription_id
 }
