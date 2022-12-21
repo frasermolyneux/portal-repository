6fedd9125fb77416c6192c87ba177045b2f7dfea
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-12-21 18:43:37 +0000

    Adding step to delete dev web app after tests have been run

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,7 @@ output "workload_public_url" {
 output "web_app_name" {
   value = azurerm_linux_web_app.app.name
 }
+
+output "web_app_resource_group" {
+  value = azurerm_linux_web_app.app.resource_group_name
+}
