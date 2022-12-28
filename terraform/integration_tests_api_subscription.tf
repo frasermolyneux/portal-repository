b43b1d8e495bdb8d4e2b8866ff2772e5d2180f3b
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-12-28 20:38:59 +0000

    Update subscription to be active state

--- terraform/integration_tests_api_subscription.tf
+++ terraform/integration_tests_api_subscription.tf
@@ -2,6 +2,8 @@ resource "azurerm_api_management_subscription" "integration_tests" {
   provider            = azurerm.api_management
   api_management_name = data.azurerm_api_management.platform.name
   resource_group_name = data.azurerm_api_management.platform.resource_group_name
+
+  state = "active"  
   
   api_id        = azurerm_api_management_api.repository_api.id
   display_name  = "Integration Tests"
