29ac8205b4bb355ea6bb7163bb98a1b2fd25e895
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-01-17 21:39:13 +0000

    Add depends on fix

--- terraform/api_management_logger.tf
+++ terraform/api_management_logger.tf
@@ -11,6 +11,10 @@ resource "azurerm_api_management_named_value" "app_insights_apim_instrumentation
   value_from_key_vault {
     secret_id = azurerm_key_vault_secret.app_insights_instrumentation_key_secret.id
   }
+
+  depends_on = [
+    azurerm_role_assignment.apim_kv_role_assignment
+  ]
 }
 
 
