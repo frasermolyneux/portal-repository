3c9d975e725a66204a82479ea993ac09191e1d0e
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-01-26 20:45:43 +0000

    Removing sql server deploy permissions

--- terraform/sql_server_deploy_permissions.tf
+++
@@ -1,9 +0,0 @@
-data "azuread_group" "sql_server_admins" {
-  display_name     = format("sg-sql-platform-%s-admins", var.environment)
-  security_enabled = true
-}
-
-resource "azuread_group_member" "deploy_principal_permissions" {
-  group_object_id  = data.azuread_group.sql_server_admins.id
-  member_object_id = data.azuread_client_config.current.object_id
-}
