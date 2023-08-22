f9d26c5a80b08951eea12b0fcc29f4b9aa62bc78
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-08-22 21:40:49 +0100

    Updating package versions

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.1.0"
+  required_version = ">= 1.5.5"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.35.0"
+      version = "~> 3.70.0"
     }
   }
 
