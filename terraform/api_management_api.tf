512230936f73abfc013c5c9799e23ea4d9d8f506
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-02-08 20:30:07 +0000

    Remove frontdoor from architecture

--- terraform/api_management_api.tf
+++ terraform/api_management_api.tf
@@ -7,7 +7,7 @@ resource "azurerm_api_management_backend" "webapi_api_management_backend" {
   protocol    = "http"
   title       = local.workload_name
   description = local.workload_name
-  url         = format("https://%s.%s", local.workload_name, var.dns_zone_name)
+  url         = format("https://%s", azurerm_linux_web_app.app.default_hostname)
 
   tls {
     validate_certificate_chain = true
--- terraform/data.frontdoor.tf
+++
@@ -1,5 +0,0 @@
-data "azurerm_cdn_frontdoor_profile" "platform" {
-  provider            = azurerm.frontdoor
-  name                = var.frontdoor_name
-  resource_group_name = var.frontdoor_resource_group_name
-}
