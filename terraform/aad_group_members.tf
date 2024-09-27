4efb60cdaa880aa86c09ae195af4931dc7a811b6
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-09-27 23:07:22 +0100

    Fixing property reference

--- terraform/aad_group_members.tf
+++ terraform/aad_group_members.tf
@@ -1,9 +1,9 @@
 resource "azuread_group_member" "web_api_database_readers" {
-  group_object_id  = azuread_group.repo_database_readers.id
+  group_object_id  = azuread_group.repo_database_readers.object_id
   member_object_id = azurerm_linux_web_app.app.identity.0.principal_id
 }
 
 resource "azuread_group_member" "web_api_database_writers" {
-  group_object_id  = azuread_group.repo_database_writers.id
+  group_object_id  = azuread_group.repo_database_writers.object_id
   member_object_id = azurerm_linux_web_app.app.identity.0.principal_id
 }
