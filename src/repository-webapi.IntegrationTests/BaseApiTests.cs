5a28faf518dac66f7cfea5cced7c08c626e4abda
Fraser Molyneux <frasermolyneux@mx-mail.io>
2025-04-25 21:02:14 +0100

    Fixing integration tests

--- src/repository-webapi.IntegrationTests/BaseApiTests.cs
+++ src/repository-webapi.IntegrationTests/BaseApiTests.cs
@@ -10,6 +10,11 @@ using XtremeIdiots.Portal.RepositoryApi.Abstractions.Interfaces;
 using XtremeIdiots.Portal.RepositoryApiClient;
 using XtremeIdiots.Portal.RepositoryApiClient.Api;
 
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.EnvironmentVariables;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using System.Reflection;
+
 namespace repository_webapi.IntegrationTests;
 
 public class BaseApiTests
@@ -17,29 +22,34 @@ public class BaseApiTests
     protected IPlayersApi playersApi;
     protected IRootApi rootApi;
 
-    [Fact]
-    public async Task SetUp()
+    public BaseApiTests()
     {
-        Console.WriteLine($"Using API Base URL: {Environment.GetEnvironmentVariable("api_base_url")}");
+        var configuration = new ConfigurationBuilder()
+            .AddUserSecrets(Assembly.GetExecutingAssembly(), true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        Console.WriteLine($"Using API Base URL: {configuration["api_base_url"]}");
 
         var mockRepositoryApiTokenProviderLogger = new Mock<ILogger<ApiTokenProvider>>();
 
-        string baseUrl = Environment.GetEnvironmentVariable("api_base_url") ?? throw new Exception("Environment variable 'api_base_url' is null - this needs to be set to invoke tests");
-        string apiKey = Environment.GetEnvironmentVariable("api_key") ?? throw new Exception("Environment variable 'api_key' is null - this needs to be set to invoke tests");
-        string apiAudience = Environment.GetEnvironmentVariable("api_audience") ?? throw new Exception("Environment variable 'api_audience' is null - this needs to be set to invoke tests");
+        string baseUrl = configuration["api_base_url"] ?? throw new Exception("Environment variable 'api_base_url' is null - this needs to be set to invoke tests");
+        string apiKey = configuration["api_key"] ?? throw new Exception("Environment variable 'api_key' is null - this needs to be set to invoke tests");
+        string apiAudience = configuration["api_audience"] ?? throw new Exception("Environment variable 'api_audience' is null - this needs to be set to invoke tests");
 
         var repositoryApiClientOptions = Options.Create(new RepositoryApiClientOptions()
         {
             BaseUrl = baseUrl,
             PrimaryApiKey = apiKey,
-            ApiAudience = apiAudience
+            ApiAudience = apiAudience,
+            ApiPathPrefix = configuration["api_path_prefix"] ?? string.Empty
         });
         var tokenProvider = new ApiTokenProvider(mockRepositoryApiTokenProviderLogger.Object, new MemoryCache(new MemoryCacheOptions()), new DefaultTokenCredentialProvider(), TimeSpan.FromMinutes(5));
 
         playersApi = new PlayersApi(new Mock<ILogger<PlayersApi>>().Object, tokenProvider, new MemoryCache(new MemoryCacheOptions()), repositoryApiClientOptions, new RestClientSingleton());
         rootApi = new RootApi(new Mock<ILogger<RootApi>>().Object, tokenProvider, repositoryApiClientOptions, new RestClientSingleton());
 
-        await WarmUp();
+        WarmUp().Wait();
     }
 
     private async Task WarmUp()
