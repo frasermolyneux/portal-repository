6c5209b39a2d738431c5f83a3025e00e4782b15c
Fraser Molyneux <frasermolyneux@mx-mail.io>
2025-06-30 21:30:20 +0100

    refactor: Remove legacy OpenAPI v2 specification and update API policy configuration

--- terraform/api_management_api_legacy.tf
+++ terraform/api_management_api_legacy.tf
@@ -40,8 +40,33 @@ resource "azurerm_api_management_product_api" "repository_api_legacy" {
   api_management_name = data.azurerm_api_management.core.name
 }
 
-// Legacy API policy is now defined in api_management_policy_updates.tf
-// as azurerm_api_management_api_policy.repository_api_policy_legacy
+resource "azurerm_api_management_api_policy" "repository_api_policy_legacy" {
+  api_name            = azurerm_api_management_api.repository_api_legacy.name
+  resource_group_name = data.azurerm_api_management.core.resource_group_name
+  api_management_name = data.azurerm_api_management.core.name
+
+  xml_content = <<XML
+<policies>
+  <inbound>
+      <base/>
+      <set-backend-service backend-id="${azurerm_api_management_backend.webapi_api_management_backend_versioned["v1"].name}" />
+      <!-- Add v1.0 path prefix to the request URL for the backend API -->
+      <rewrite-uri template="/${local.backend_mapping["v1"].api_path}/v1.0@(context.Request.OriginalUrl.Path.Substring(context.Api.Path.Length))" />
+  </inbound>
+  <backend>
+      <forward-request />
+  </backend>
+  <outbound>
+      <base/>
+  </outbound>
+  <on-error />
+</policies>
+XML
+
+  depends_on = [
+    azurerm_api_management_backend.webapi_api_management_backend_versioned
+  ]
+}
 
 resource "azurerm_api_management_api_diagnostic" "repository_api_diagnostic_legacy" {
   identifier               = "applicationinsights"
