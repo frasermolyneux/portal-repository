a1fa6f8c878614513be125a2e50ab023e2608b8b
Fraser Molyneux <frasermolyneux@mx-mail.io>
2025-08-24 07:22:58 +0100

    Add full-text catalog and indexes for ChatMessages, Players, and PlayerAlias

--- src/XtremeIdiots.Portal.Repository.Database/Scripts/Script.PostDeployment.sql
+++ src/XtremeIdiots.Portal.Repository.Database/Scripts/Script.PostDeployment.sql
@@ -18,3 +18,46 @@ Post-Deployment Script Template
 
 -- Include Tags script
 :r .\Script.PostDeploymentTags.sql
+
+-- Idempotent Full Text setup (catalog + indexes) - safe if objects already exist
+IF NOT EXISTS (SELECT 1 FROM sys.fulltext_catalogs WHERE name = 'PortalFTCatalog')
+BEGIN
+    PRINT 'Creating full-text catalog PortalFTCatalog';
+    CREATE FULLTEXT CATALOG PortalFTCatalog AS DEFAULT;
+END
+
+IF NOT EXISTS (
+    SELECT 1 FROM sys.fulltext_indexes fi
+    JOIN sys.objects o ON fi.object_id = o.object_id
+    WHERE o.name = 'ChatMessages')
+BEGIN
+    PRINT 'Creating FT index on ChatMessages';
+    CREATE FULLTEXT INDEX ON [dbo].[ChatMessages] ([Username] LANGUAGE 1033, [Message] LANGUAGE 1033)
+        KEY INDEX [PK_dbo.ChatMessage]
+        ON [PortalFTCatalog]
+        WITH CHANGE_TRACKING AUTO;
+END
+
+IF NOT EXISTS (
+    SELECT 1 FROM sys.fulltext_indexes fi
+    JOIN sys.objects o ON fi.object_id = o.object_id
+    WHERE o.name = 'Players')
+BEGIN
+    PRINT 'Creating FT index on Players';
+    CREATE FULLTEXT INDEX ON [dbo].[Players] ([Username] LANGUAGE 1033, [Guid] LANGUAGE 1033)
+        KEY INDEX [PK_dbo.Players]
+        ON [PortalFTCatalog]
+        WITH CHANGE_TRACKING AUTO;
+END
+
+IF NOT EXISTS (
+    SELECT 1 FROM sys.fulltext_indexes fi
+    JOIN sys.objects o ON fi.object_id = o.object_id
+    WHERE o.name = 'PlayerAlias')
+BEGIN
+    PRINT 'Creating FT index on PlayerAlias';
+    CREATE FULLTEXT INDEX ON [dbo].[PlayerAlias] ([Name] LANGUAGE 1033)
+        KEY INDEX [PK_dbo.PlayerAlias]
+        ON [PortalFTCatalog]
+        WITH CHANGE_TRACKING AUTO;
+END
