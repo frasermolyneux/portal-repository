049f63c7a34abbacdfb0c5c30ee8e59194f83976
Fraser Molyneux <frasermolyneux@mx-mail.io>
2025-08-26 17:59:01 +0100

    Add root endpoint to API for health check response

--- src/XtremeIdiots.Portal.Repository.Api.V1/Program.cs
+++ src/XtremeIdiots.Portal.Repository.Api.V1/Program.cs
@@ -135,4 +135,9 @@ app.UseAuthorization();
 app.MapControllers();
 app.MapHealthChecks("/api/health").AllowAnonymous();
 
+
+app.MapGet("/", () => Results.Ok("OK"))
+   .WithName("Root")
+   .AllowAnonymous();
+
 app.Run();
