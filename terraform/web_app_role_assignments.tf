2800c4b82f13eb34fddbdbe952738c1d93ac0e2a
Fraser Molyneux <frasermolyneux@mx-mail.io>
2025-10-04 15:52:22 +0100

    Refactor web app role assignments to consolidate and rename resources for clarity

--- terraform/web_app_role_assignments.tf
+++ terraform/web_app_role_assignments.tf
@@ -1,22 +1,10 @@
-resource "azurerm_role_assignment" "web_app_kv_role_assignment_v1" {
+resource "azurerm_role_assignment" "repository_webapi_identity_principal_id_to_key_vault" {
   scope                = azurerm_key_vault.kv.id
   role_definition_name = "Key Vault Secrets User"
   principal_id         = local.repository_webapi_identity_principal_id
 }
 
-resource "azurerm_role_assignment" "app-to-storage_v1" {
-  scope                = azurerm_storage_account.app_data_storage.id
-  role_definition_name = "Storage Blob Data Owner"
-  principal_id         = local.repository_webapi_identity_principal_id
-}
-
-resource "azurerm_role_assignment" "web_app_kv_role_assignment_v2" {
-  scope                = azurerm_key_vault.kv.id
-  role_definition_name = "Key Vault Secrets User"
-  principal_id         = local.repository_webapi_identity_principal_id
-}
-
-resource "azurerm_role_assignment" "app-to-storage_v2" {
+resource "azurerm_role_assignment" "repository_webapi_identity_principal_id_to_app_data_storage" {
   scope                = azurerm_storage_account.app_data_storage.id
   role_definition_name = "Storage Blob Data Owner"
   principal_id         = local.repository_webapi_identity_principal_id
