09e6b62d7fb0004b5e7314ec4b97fb5330eb274a
Fraser Molyneux <frasermolyneux@mx-mail.io>
2026-04-26 21:21:42 +0100

    fix: correct formatting of GameServerEvents table definition

--- src/XtremeIdiots.Portal.Repository.Database/dbo/Tables/GameServerEvents.sql
+++ src/XtremeIdiots.Portal.Repository.Database/dbo/Tables/GameServerEvents.sql
@@ -1,9 +1,10 @@
-﻿CREATE TABLE [dbo].[GameServerEvents] (
-	[GameServerEventId] UNIQUEIDENTIFIER NOT NULL  DEFAULT newsequentialid(), 
-    [GameServerId] UNIQUEIDENTIFIER NOT NULL, 
-    [Timestamp] DATETIME NOT NULL, 
-    [EventType] VARCHAR(50) NOT NULL, 
-    [EventData] VARCHAR(MAX) NULL, 
+﻿CREATE TABLE [dbo].[GameServerEvents]
+(
+    [GameServerEventId] UNIQUEIDENTIFIER NOT NULL DEFAULT newsequentialid(),
+    [GameServerId] UNIQUEIDENTIFIER NOT NULL,
+    [Timestamp] DATETIME NOT NULL,
+    [EventType] VARCHAR(50) NOT NULL,
+    [EventData] VARCHAR(MAX) NULL,
     CONSTRAINT [PK_GameServerEvents] PRIMARY KEY ([GameServerEventId]),
     CONSTRAINT [FK_GameServerEvents_GameServerId] FOREIGN KEY ([GameServerId]) REFERENCES [dbo].[GameServers] ([GameServerId])
 );
